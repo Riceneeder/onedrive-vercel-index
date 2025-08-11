9dcd7db8daec88ec0ef2cfda3387a65e979d40b1
Zhengkun Gan <86492950+Riceneeder@users.noreply.github.com>
2025-08-11 12:12:42 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,14 +17,14 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Postman1year's OneDrive",
+  title: "ZhengKun Gan's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: process.env.BASE_DIRECTORY || '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 50,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
@@ -38,26 +38,28 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/postman1year/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by postman1year.',
+    'Shared from <a href="https://gankun.cn.ma" target="_blank" rel="noopener noreferrer">Riceneeder</a>.Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo & postman1year.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
-    '/㊙️ Private',
-    '/🎵 Music',
-    '/🥁 Taiko/🎶 TJA/🎵 Real TJA'
+
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:postman1year@gmail.com',
+  email: 'mailto:845541909@qq.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
+      name: 'Riceneeder',
+      link: 'https://gankun.cn.ma',
+    },
+    {
       name: 'GitHub',
-      link: 'https://github.com/postman1year/onedrive-vercel-index',
+      link: 'https://github.com/Riceneeder/',
     },
   ],
 
@@ -66,3 +68,4 @@ module.exports = {
   // without timezone and replacing T with space.
   datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
 }
+
